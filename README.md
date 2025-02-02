f9b0c117f98fa9f86f3a93ec2406d4d4a23ee9b0
Tushar Tyagi <tushartyagi0507@gmail.com>
2025-02-02 15:19:50 +0530

    Set up ready with tailwind and daisy

---
+++ README.md
@@ -0,0 +1,11 @@
+# DevTinder Frontend featurs and design
+
+## Features
+
+- Login
+- Signup
+- Profile
+- Feed
+- Match/Connections
+- Chat
+- sending connection request
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold underline text-red-600">Hello world</p>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </>
+  );
+}
+
+export default App;
